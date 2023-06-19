f5b4705a56dc6bbab616250e2665435a26acf7cf
welangai eric mmasi <welangaieric@gmail.com>
2023-06-19 18:15:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post('/send',(req,res)=>{
         'Content-Type': 'application/json',
         'Authorization': 'Bearer MGaGkWC5sSemjkDi9cBKQYqN2ZlL'
     })
-    'send(JSON.stringify({
+    .send(JSON.stringify({
         "BusinessShortCode": 8591268,
         "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjMwNjE5MTYyMTA0",
         "Timestamp": "20230619162104",
@@ -29,7 +29,7 @@ app.post('/send',(req,res)=>{
         "PartyA":req.body.name ,
         "PartyB":8591268,
         "PhoneNumber": req.body.phone,
-        "CallBackURL": "https://mpesa.onrender.com/",
+        "CallBackURL": "https://mpesa-lht1.onrender.com/",
         "AccountReference": "Sneaker kenya",
         "TransactionDesc": "Payment of sneakers " 
       }))
